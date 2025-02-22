8268fd75692dbfaca5bf6d69e7f8ff74bc084217
mayanimahek007 <mayanimahek007@gmail.com>
2025-02-22 23:45:34 +0530

    chnge line

--- wedmegood-admin/src/Components/Header/header.js
+++ wedmegood-admin/src/Components/Header/header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div  >
             <a href="https://webpixels.io/components?ref=codepen" class="btn w-full text-truncate rounded-0 py-2 hdr_admin border-0 position-relative" style={{ ZIndex: "1000" }}>
-                <strong>Crafted with Webpixels CSS:</strong> The design system for Bootstrap 5. Browse all components →
+                <strong>Optimized for efficiency — manage everything with ease.</strong>
             </a>
 
         </div>
